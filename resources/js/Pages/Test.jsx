10a4336713da92716537ab22ae8f1c02d38ef9f9
Nahid Hasan Sourav <nahidhasansourav@gmail.com>
2025-06-15 23:48:35 +0600

    install all necessary packages

---
+++ resources/js/Pages/Test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Test = () => {
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gray-600">
+            <h1 className="text-3xl font-bold text-blue-600">This is test home page</h1>
+        </div>
+    );
+};
+
+export default Test;
